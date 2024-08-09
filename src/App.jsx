e54eec91b13855ab3463a5c2e3a24271dbbba51f
Anna <iaanya27@gmail.com>
2024-08-09 11:27:42 +0300

    added useEffect and Access Key

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,42 @@
-// import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import axios from "axios";
 import './App.css'
 
 function App() {
-  // const [count, setCount] = useState(0);
+  const [images, setImages] = useState(null);
 
 
+  // Access Key
+  // TNyf12i8jeVlVVDDWlHnde9oQXes7SAdWsyNp-n8EXQ
+  
+  // https://api.unsplash.com/photos/?client_id=YOUR_ACCESS_KEY
 
-  axios.get('/users')
-    .then((res) => res.json())
-    .then(console.log);
+  useEffect(() => {
+    const fetchImg = async () => {
+      const { data } =
+        await axios.get('/users');
+      setImages(data.images)
+    };
+    fetchImg();
+    }, []);
 
   return (
     <>
-            <h1>Image Gallery</h1>
+      <h1>Image Gallery</h1>
       <div className="card">
+        {Array.isArray(images) && images.map(image => {
+          return (
+            <div key={image.id}>
+              <h3>{image.title}</h3>
+              <img>{image.image}</img>
+            </div>
+          );
+        })}
         
-      </div>
+      </div>;
       
     </>
-  )
-}
+  );
+};
 
 export default App
